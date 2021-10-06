b4b960a2fce994bf2e7e1c21f9f83b56e4d3c452
Lewin Probst <info@emirror.de>
2021-10-06 19:37:59 +0200

    Added FileHierarchyBuilder, added collect_filenames to FileHierarchy.

--- src/fh/mod.rs
+++ src/fh/mod.rs
@@ -1,6 +1,10 @@
 //! Defines the file hierarchy of [lewp](crate).
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+
+mod builder;
+
+pub use builder::FileHierarchyBuilder;
 
 /// Possible file types that the file hierarchy is covering.
 pub enum FileType {
@@ -43,6 +47,46 @@ impl FileHierarchy {
         path
     }
 
+    /// Collects all filenames recursively in the given subfolder. `subfolder`
+    /// is referenced to the base directory given in the FileHierarchy instance.
+    /// Parts containing `../` are removed before processing.
+    fn collect_filenames(&self, subfolder: &str) -> Vec<PathBuf> {
+        let isolated_subfolder = self.isolate_path(subfolder);
+        let subfolder = self.base_directory.join(Path::new(&isolated_subfolder));
+        if !subfolder.is_dir() {
+            return vec![];
+        }
+        let mut filenames = vec![];
+        for entry in walkdir::WalkDir::new(&subfolder) {
+            let entry = match entry {
+                Ok(v) => v.into_path(),
+                Err(_) => continue,
+            };
+            if entry.is_dir() {
+                continue;
+            }
+            let entry = match self.remove_base_dir(&subfolder, &entry) {
+                Ok(p) => p,
+                Err(msg) => {
+                    log::error!("{}", msg);
+                    continue;
+                }
+            };
+            filenames.push(entry)
+        }
+        filenames
+    }
+
+    fn remove_base_dir(&self, base_dir: &Path, input_path: &Path) -> Result<PathBuf, String> {
+        match pathdiff::diff_paths(input_path, base_dir) {
+            Some(p) => Ok(p),
+            None => match input_path.to_str() {
+                Some(v) => Err(format!("Could not remove base dir of {}", v)),
+                None => Err("Could not remove base dir!".to_string()),
+            },
+        }
+    }
+
     /// Returns the correct extension for the given file type.
     pub(crate) fn extension(&self, file_type: FileType) -> &str {
         match file_type {
@@ -58,6 +102,15 @@ impl FileHierarchy {
             Level::Module => "modules",
         }
     }
+
+    /// Removes `../` from the given string to isolate the filepath to a base
+    /// directory.
+    fn isolate_path(&self, path: &str) -> String {
+        let s = String::from(path);
+        let mut s = s.split('/').collect::<Vec<&str>>();
+        s.retain(|&e| !e.contains(".."));
+        s.join("/")
+    }
 }
 
 impl Default for FileHierarchy {
@@ -67,7 +120,7 @@ impl Default for FileHierarchy {
 }
 
 #[test]
-fn folder_creation() {
+fn folder_name_generation() {
     let fh = FileHierarchy::new();
     assert_eq!(
         "./modules/module-id/css",
@@ -81,4 +134,28 @@ fn folder_creation() {
             .to_str()
             .unwrap()
     );
+}
+
+#[test]
+fn isolate_file_paths() {
+    let fh = FileHierarchyBuilder::new().build();
+    let breakout = "../something";
+    let isolated = fh.isolate_path(breakout);
+    assert_eq!(isolated, "something");
+    let non_breakout = "something/subfolder";
+    let isolated = fh.isolate_path(non_breakout);
+    assert_eq!(isolated, "something/subfolder");
+}
+
+#[test]
+fn collect_filenames() {
+    let fh = FileHierarchyBuilder::new()
+        .base_directory(PathBuf::from("testfiles"))
+        .build();
+    let mut filenames = fh.collect_filenames("");
+    let mut reference = vec![
+        PathBuf::from("modules/hello-world/css/primary.css"),
+        PathBuf::from("modules/hello-world/css/secondary.css"),
+    ];
+    assert_eq!(filenames.sort(), reference.sort());
 }
