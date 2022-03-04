6b4fc2513409aa66101a2320be90f3388fcae452
Lewin Probst <info@emirror.de>
2022-03-04 10:18:51 +0100

    fix(html): Wrong template_contents initialization

--- lewp-html/src/api.rs
+++ lewp-html/src/api.rs
@@ -239,7 +239,7 @@ fn new_element(tag_name: &str, children: Nodes) -> Node {
     let node = rcdom::Node::new(rcdom::NodeData::Element {
         name: QualName::new(None, ns!(html), LocalName::from(tag_name)),
         attrs: RefCell::new(vec![]),
-        template_contents: RefCell::new(None),
+        template_contents: None,
         mathml_annotation_xml_integration_point: false,
     });
     node.append_children(children);
